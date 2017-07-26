44ce4a874e2ac791da98fec5abf59d0a3b0bcce9
Joel Abrahams <abrahamsjo@gmail.com>
2017-07-26 16:23:10 +0200

    Fix spelling error in ant-plus.js

--- ant-plus.js
+++ ant-plus.js
@@ -3,7 +3,7 @@ var Ant = require('./build/ant');
 var HRS = require('./build/heart-rate-sensors');
 var SSD = require('./build/stride-speed-distance-sensors');
 var SC = require('./build/speed-cadence-sensors');
-var PS = require('./build/bicyle-power-sensor');
+var PS = require('./build/bicycle-power-sensor');
 
 module.exports = {
 	GarminStick2: Ant.GarminStick2,
@@ -14,6 +14,6 @@ module.exports = {
 	StrideSpeedDistanceScanner: SSD.StrideSpeedDistanceScanner,
 	SpeedCadenceSensor: SC.SpeedCadenceSensor,
 	SpeedCadenceScanner: SC.SpeedCadenceScanner,
-	BicylePowerSensor: PS.BicylePowerSensor,
+	BicylePowerSensor: PS.BicyclePowerSensor,
 	BicyclePowerScanner: PS.BicyclePowerScanner
 };
